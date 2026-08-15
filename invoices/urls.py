2f9befb96a97f5e4decf00e137611dffd674345b
Yasin <yasinthefiend2020@gmail.com>
2026-08-15 14:08:04 +0600

    Add Invoice report and Summary endpoint with sales aggregation

--- invoices/urls.py
+++ invoices/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import InvoiceViewSet
+from .views import InvoiceViewSet, InvoiceReportView
+from django.urls import path
 
 router = DefaultRouter()
 router.register('invoices', InvoiceViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls +[
+    path('reports/invoice/', InvoiceReportView.as_view(), name='invoice-report'),
+]
--- invoices/views.py
+++ invoices/views.py
@@ -5,6 +5,13 @@ from .serializers import InvoiceSerializer
 from inventory.permissions import IsStaffOrAdminOrReadOnly
 
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Sum, Count, F
+from .models import Invoice, InvoiceItem
+
+
+
 # Create your views here.
 class InvoiceViewSet(viewsets.ModelViewSet):
     queryset = Invoice.objects.all()
@@ -13,4 +20,26 @@ class InvoiceViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+
+
+class InvoiceReportView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        total_invoices = Invoice.objects.count()
+
+        total_sales = InvoiceItem.objects.aggregate(
+            total=Sum(F('quantity') * F('price'))
+        )['total'] or 0
+
+        total_products_sold = InvoiceItem.objects.aggregate(
+            total=Sum('quantity')
+        )['total'] or 0
+
+        return Response({
+            'total_invoices': total_invoices,
+            'total_sales': total_sales,
+            'total_products_sold': total_products_sold,
+        })
+
 
